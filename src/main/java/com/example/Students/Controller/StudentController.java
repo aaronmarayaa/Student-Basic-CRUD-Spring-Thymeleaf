27eb6ff9a35cc43828182738ec82299722ba4e6f
Aaron Maraya <aaronmaraya7@gmail.com>
2023-12-18 20:54:55 +0800

    feat: Add PostMapping to create new student.
    file :
    - ../Controller/StudentController.java

--- src/main/java/com/example/Students/Controller/StudentController.java
+++ src/main/java/com/example/Students/Controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.Students.Controller;
 
+import com.example.Students.Model.Student;
+import com.example.Students.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student-management/home")
 public class StudentController
 {
+    @Autowired
+    StudentService studentService;
+
     @GetMapping
     public String home() {
         return "index";
+    }
+
+    // Create Student
+    @PostMapping("/student-save")
+    public String createStudent(Student student)
+    {
+        studentService.saveStudent(student);
+
+        return "redirect:/student-management/home";
     }
 }
