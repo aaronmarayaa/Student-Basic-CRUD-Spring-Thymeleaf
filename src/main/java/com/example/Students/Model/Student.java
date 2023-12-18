0598a0fc066427eef25111cb54cdd7d93cd09d9f
Aaron Maraya <aaronmaraya7@gmail.com>
2023-12-18 19:32:37 +0800

    ref: Add setter annotation.
    file :
    -../Model/Student.java

--- src/main/java/com/example/Students/Model/Student.java
+++ src/main/java/com/example/Students/Model/Student.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name = "profile")
 public class Student
