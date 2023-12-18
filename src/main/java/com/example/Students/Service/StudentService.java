ddcd7d782c9cbb5577b47177dc7957a1aaa4b7f7
Aaron Maraya <aaronmaraya7@gmail.com>
2023-12-18 19:34:53 +0800

    Create StudentService class.
    file :
    ../Service/StudentService.java

---
+++ src/main/java/com/example/Students/Service/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.Students.Service;
+
+import com.example.Students.Model.Student;
+import com.example.Students.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService
+{
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student saveStudent(Student student) {
+        try {
+            studentRepository.save(student);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("Email already exists");
+            // Or handle the error in a way that makes sense for your application
+        }
+        return student;
+    }
+}
