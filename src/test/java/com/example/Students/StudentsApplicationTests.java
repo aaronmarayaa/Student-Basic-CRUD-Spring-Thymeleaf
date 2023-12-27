21ae660c1095bb38b720f5f1b4d165ef4e101edd
Aaron Maraya <aaronmaraya7@gmail.com>
2023-12-27 22:52:55 +0800

    Test the saving method.
    Path(../test/java/com/example/students/) :
    - StudentsApplicationTests.java

--- src/test/java/com/example/Students/StudentsApplicationTests.java
+++ src/test/java/com/example/Students/StudentsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.Students;
 
+import com.example.Students.Model.Student;
+import com.example.Students.Service.StudentService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentsApplicationTests {
 
+	@Autowired
+	StudentService studentService;
+
 	@Test
-	void contextLoads() {
+	public void saveStudentTest()
+	{
+		Student student = studentService.get(1);
+		student.setPassword("thor123");
+		Student saveStudent1 = studentService.saveStudent(student);
+
+		Assertions.assertThat(saveStudent1).isNotNull();
+		Assertions.assertThat(saveStudent1.getId()).isGreaterThan(0);
 	}
 
 }
