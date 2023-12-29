d6983039c9b0c4c1c2ac5e2f0f9c4dfb2ae43fe3
Aaron Maraya <aaronmaraya7@gmail.com>
2023-12-29 22:25:23 +0800

    Take the javascript logics from student_form.html
    file :
    - ../resources/static/js/student_form.js

---
+++ src/main/resources/static/js/student_form.js
@@ -0,0 +1,37 @@
+function togglePasswordVisibility() {
+    var passwordInputs = document.getElementsByClassName("passwordInput");
+    var showPasswordBtn = document.getElementById("showPasswordBtn");
+
+    for (var i = 0; i < passwordInputs.length; i++) {
+        if (passwordInputs[i].type === "password") {
+        passwordInputs[i].type = "text";
+        } else {
+        passwordInputs[i].type = "password";0
+        }
+    }
+
+    var img = showPasswordBtn.querySelector('img');
+    img.src = (passwordInputs[0].type === "password") ? "/images/eye-close.png" : "/images/eye-open.png";
+}
+
+function validatePasswords() {
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var errorMessageElement = document.getElementById("errorMessage");
+    var studentForm = document.getElementById('studentForm');
+
+    if (newPassword !== confirmPassword) {
+        errorMessageElement.textContent = "Passwords do not match. Please try again.";
+        
+        errorMessageElement.style.display = 'block';
+        
+        setTimeout(() => {
+        errorMessageElement.style.display = 'none';
+        }, 3000);
+
+        return false;
+    } else {
+        errorMessageElement.textContent = "";
+        return true;
+    }
+}
